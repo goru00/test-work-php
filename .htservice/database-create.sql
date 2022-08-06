6520d950bc561e653084369b2e30cdc19f7e31e4
KozinIvan <kozinivan@ki-corporation.ru>
2022-08-06 16:56:24 +0300

    Инициализация репозитория

---
+++ .htservice/database-create.sql
@@ -0,0 +1,3 @@
+--CREATE DATABASE `test_work_db` COLLATE 'utf8mb4_general_ci';
+USE `test_work_db`;
+
---
+++ .htservice/recreate-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Recreate the database.
+
+echo "Recreating the database..."
+
+echo "Drop database..." & mysql -h localhost -u test_work_db -p20IgodJ00eTY83D7 < database-drop.sql
+echo "Create database..." & mysql -h localhost -u test_work_db -p20IgodJ00eTY83D7 < database-create.sql
+echo "Import data..." & mysql -h localhost -u test_work_db -p20IgodJ00eTY83D7 < database-insert.sql
+
+echo "Done."
