7347b39a4c84bdaec3ea3a135dfd685fde36e29c
KozinIvan <kozinivan@ki-corporation.ru>
2022-08-07 17:25:34 +0300

    Загрузка готового задания

---
+++ engine/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('root_define')) die('Access denied'); // Проверяем источник скрипта
+
+$Config = [
+    'db' => [
+        'host' => 'localhost:3306', // Хост базы данных
+        'user' => 'test_user_db', // Имя пользователя базы данных
+        'pass' => '20IgodJ00eTY83D7', // Пароль пользователя базы данных
+        'name' => 'test_work_db' // Имя базы данных
+    ],
+];
